85a5d8d89dde9f21ed428275bae8dd42b6fb1ac4
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-06-17 17:21:40 -0300

    URLify a given string (Replace spaces with %20)

--- src/com/cracking/ArrayAndStrings.java
+++ src/com/cracking/ArrayAndStrings.java
@@ -83,35 +83,48 @@ public class ArrayAndStrings {
   }
 
   public static char[] urlFy(char[] str) {
-    char vazio = ' ';
 
-    for (int i = 0; i < str.length; i++) {
-      int count_empty = 0;
-      if (str[i] == vazio) {
-        count_empty++;
-      }
-      int new_length = i + count_empty * 2;
+    int MAX = 1000;
 
-      int index = new_length - 1;
+    // count spaces and find current length
+    int space_count = 0, i = 0;
+    for (i = 0; i < str.length; i++) if (str[i] == ' ') space_count++;
 
-      char[] old_str = str;
+    // count spaces and find current length
+    while (str[i - 1] == ' ') {
+      space_count--;
+      i--;
+    }
 
-      str = new char[new_length];
+    // Find new length.
+    int new_length = i + space_count * 2;
 
-      for (int j = i - 1; j >= 0; j--) {
+    // New length must be smaller than length
+    // of string provided.
+    if (new_length > MAX) return str;
 
-        if (old_str[j] == ' ') {
-          str[index] = '0';
-          str[index - 1] = '2';
-          str[index - 2] = '%';
-          index = index - 3;
-        } else {
+    // Start filling character from end
+    int index = new_length - 1;
 
-          str[index] = old_str[j];
-          index--;
-        }
+    char[] old_str = str;
+    str = new char[new_length];
+
+    // Fill rest of the string from end
+    for (int j = i - 1; j >= 0; j--) {
+
+      // inserts %20 in place of space
+      if (old_str[j] == ' ') {
+        str[index] = '0';
+        str[index - 1] = '2';
+        str[index - 2] = '%';
+        index = index - 3;
+      } else {
+        str[index] = old_str[j];
+        index--;
       }
     }
     return str;
   }
+
+  //public static palindrom
 }
